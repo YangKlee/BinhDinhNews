c10658ab1d3d9c3050fcd26c8c47ca165bb4b0e1
Khan <huynhkhang1407@gmail.com>
2025-03-14 21:41:04 +0700

    Footer(Demo)

--- index.php
+++ index.php
@@ -145,8 +145,17 @@
             <div></div>
         </div>
     </div>
-    <footer>
-
+    <footer class="footer">
+        <div class="footer-left">
+            <p>Website: <a href="https://www.binhdinhtv.vn" style="color: white;">www.binhdinhtv.vn</a></p>
+            <p>Email: <a href="mailto:contact@binhdinhnews.vn" style="color: white;">contact@binhdinhnews.vn</a></p>
+            <p>Số điện thoại: 0123-456-789</p>
+            <p>Địa chỉ: 123 Đường ABC, Thành phố Quy Nhơn, Tỉnh Bình Định</p>
+        </div>
+        <div class="footer-right">
+            <img src="scr/img/Logo-Tinh-Binh-Dinh.webp" alt="Logo" class="footer-logo">
+            <p>CỔNG THÔNG TIN BÌNH ĐỊNH</p>
+        </div>
     </footer>
 </body>
 </html>
