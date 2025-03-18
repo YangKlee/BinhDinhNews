6c6980a1af52e065595fb57f78c9c67034eee144
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-03-18 23:53:09 +0700

    phán đấu làm lốp trưởng xuất sắc cấp trường

--- app/views/home-page.php
+++ app/views/home-page.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="./css/reset.css">
 	<link rel="stylesheet" href="./css/footer-style.css">
 	<link rel="stylesheet" href="./css/header-style.css">
+    <script src="https://kit.fontawesome.com/8f5e4d2946.js" crossorigin="anonymous"></script>
+
     <title>Trang chủ</title>
 </head>
 <?php
@@ -14,30 +16,37 @@
 <body>
     <div class="homepage homepage-container">
         <div class="container__content">
-            <div class="homepage hot-news">
+            <div class="homepage-content hot-news">
+                <i class="fa-solid fa-star"></i>
                 <h1>Tin nóng</h1>
                 <hr>
             </div>
-            <div class="homepage current-news">
+            <div class="homepage-content current-news">
+                <i class="fa-solid fa-square-rss"></i>
                 <h1>Tin mới</h1>
                 <hr>
             </div>
-            <div class="homepage thoi-su">
+            <div class="homepage-content thoi-su">
+                <i class="fa-solid fa-globe"></i>
                 <h1>Thời sự<h1>
                 <hr>
             </div>
-            <div class="homepage kinh-te">
+            <div class="homepage-content kinh-te">
+                <i class="fa-solid fa-coins"></i>
                 <h1>Kinh tế - tổng hợp<h1>
                 <hr>
             </div>
  
 
-            <div class="homepage van-hoa">
+            <div class="homepage-content van-hoa">
+                <i class="fa-solid fa-earth-americas"></i>
                 <h1>Văn hóa<h1>
                 <hr>
             </div>
-            <div class="homepage van-hoa">
-                <p>Du lịch</p>
+            <div class="homepage-content du-lich">
+                <i class="fa-solid fa-plane"></i>
+                <h1>Du lịch<h1>
+                <hr>
             </div>
 
             
