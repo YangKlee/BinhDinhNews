248da4ad0f30f4f94a4353f677c19c2665b51f52
Khan <huynhkhang1407@gmail.com>
2025-03-18 21:53:11 +0700

    cuong gay

--- module/footer.php
+++ module/footer.php
@@ -4,16 +4,26 @@
         <link rel="stylesheet" href="./module/src/reset.css">
     </head>
     <body>
-        <footer class="footer">
+        <footer class="footer">            
             <div class="footer-left">
-                <p>Website: <a href="https://www.binhdinhtv.vn" style="color: white;">www.binhdinhtv.vn</a></p>
-                <p>Email: <a href="mailto:contact@binhdinhnews.vn" style="color: white;">contact@binhdinhnews.vn</a></p>
-                <p>Số điện thoại: 0123-456-789</p>
-                <p>Địa chỉ: 123 Đường ABC, Thành phố Quy Nhơn, Tỉnh Bình Định</p>
-            </div>
-            <div class="footer-right">
-                <img src="./module/img/logo.webp" alt="Logo" class="footer-logo">
-                <p>CỔNG THÔNG TIN BÌNH ĐỊNH</p>
+                <div class="head">
+                    <div><img src="./module/img/logo.webp" alt="Logo" class="footer-logo"></div>
+                    <div>                    
+                        <h2>
+                        <span>© CỔNG THÔNG TIN BÌNH ĐỊNH</span>
+                        </h2>
+                        <p>
+                        Chủ tịch Trần Thanh Cường, Thư kí Nguyễn Khánh Dương
+                        </p>
+                </div>
+                </div>
+
+                <div class="footer-content">
+                    <p>Website: www.binhdinhnews.vn</p>
+                    <p>Email: binhdinhnews@binhdinh.vn</p>
+                    <p>Số điện thoại: 0123-456-789</p>
+                    <p>Địa chỉ: 123 Đường ABC, Thành phố Quy Nhơn, Tỉnh Bình Định</p>
+                </div>
             </div>
         </footer>
     </body>
