b095a0cafcbd0b0e4b971cce13682193ddd02609
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-03-19 14:42:34 +0700

    anh đi xe wawe nên em chỉ coi anh là michelin boy

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -1,3 +1,31 @@
 function showArticle(idArt) {
     window.location.href = `./article.php?id=${idArt}`
 }
+function loadHotNews()
+{
+    let listArt =  document.getElementsByClassName("hot-article")
+    console.log(listArt);
+    for (let i = 1; i < listArt.length; i++)
+    {
+        listArt[i].style.display = "none";
+    }
+}
+let index_list_Hot_art = 0;
+
+function loadArtNext(isNext){
+    let lenght_list_Hot_art = document.getElementsByClassName("hot-article").length;
+    let listHotArt =document.getElementsByClassName("hot-article");
+    listHotArt[index_list_Hot_art].style.display = "none";
+    if (isNext) index_list_Hot_art++;
+    else index_list_Hot_art--;
+    console.log( document.getElementsByClassName("hot-article").length);
+    if (index_list_Hot_art > lenght_list_Hot_art -1)
+    {
+        index_list_Hot_art = 0;
+    }
+    else if(index_list_Hot_art < 0){
+        index_list_Hot_art = lenght_list_Hot_art -1 ;
+    }
+    listHotArt[index_list_Hot_art].style.display = "block";
+
+}
