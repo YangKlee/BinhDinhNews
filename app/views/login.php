be4c9c8efde9d6d72c7ec752d5e10317e6194d3b
Cuongpipe <126488715+cuonglqc@users.noreply.github.com>
2025-03-20 14:44:50 +0700

    Update login.php
    sv

--- app/views/login.php
+++ app/views/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trang Đăng Nhập</title>
+    <title> Trang Đăng Nhập </title>
     <link rel="stylesheet" href="../../public/css/login-style.css">
 </head>
 <body>
