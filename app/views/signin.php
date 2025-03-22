8b1207f9e3c228e78dd2a70003f0101b25096b75
Cường <tranthanhcuongit@gmail.com>
2025-03-22 20:42:47 +0700

    Sigin
    Task:
    + Thêm Sigin
    + Sửa lại login và sigin nhìn cho hợp lí hơn
    + Đã link
    + ÁUUUUUUUUUUUUUUUUUUUUUUUUUU
    * Mình ta oanh tạc nơi chiến trận *
    * Nàng ta ở nhà phá lỗ đái cùng ai *

--- app/views/signin.php
+++ app/views/signin.php
@@ -3,22 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Trang Đăng Nhập </title>
-    <link rel="stylesheet" href="../../public/css/login-style.css">
+    <title> Trang Đăng Ký </title>
+    <link rel="stylesheet" href="../../public/css/signin-style.css">
 </head>
 <body>
     <form>
-        <h3>Đăng nhập</h3>
+        <h3>Đăng ký</h3>
 
         <label for="username">Tên đăng nhập</label>
         <input type="text" placeholder="Email hoặc Tên Đăng Nhập" id="username">
-
         <label for="password">Mật khẩu</label>
         <input type="password" placeholder="Mật khẩu" id="password">
+        <label for="password">Xác nhận mật khẩu</label>
+        <input type="password" placeholder="Nhập lại mật khẩu lần 2" id="password">
 
-        <button>Đăng nhập</button>
-        <a href="signin.php">Chưa có tài khoản ?</a>
-        <a href="#">Quên mật khẩu ?</a>
+        <button>Đăng ký</button>
+        
+        <a href="login.php">Đã có tài khoản ?</a>
         
     </form>
 </body>
