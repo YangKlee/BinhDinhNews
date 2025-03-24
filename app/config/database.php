9133a3baadb860a2d2c7b8c1781f779e27c86fb5
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-03-24 22:38:16 +0700

    aaaaaaaaaaaaaa

---
+++ app/config/database.php
@@ -0,0 +1,19 @@
+<?php
+    class DatabaseConnection{
+        private $nameserver = 'localhost';
+        private $username = 'root';
+        private $password = '';
+        private $dbname = 'binhdinhnews_de';
+
+        public function getConnection()
+        {
+            $conn = new mysqli($this->nameserver, $this->username, $this->password, $this->dbname);
+            return $conn;
+        }
+        public function closeConnection($conn)
+        {
+            $conn->closeConnection();
+        }
+
+    }
+?>
