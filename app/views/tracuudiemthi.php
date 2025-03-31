2c9df4550f51ea631033b2374eba69eb18859862
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-03-31 20:52:05 +0700

    qq

--- app/views/tracuudiemthi.php
+++ app/views/tracuudiemthi.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TRA CỨU ĐIỂM THI THPT QUỐC GIA</title>
-    <link rel="stylesheet" href="stylestracuu.css">
+    <link rel="stylesheet" href="../../public/css/stylestracuu.css">
 
 </head>
 <body>
