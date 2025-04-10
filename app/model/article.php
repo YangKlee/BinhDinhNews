09b76d47ab26f50bcb0c4d2bad9c8e586800f674
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-04-10 15:36:03 +0700

    upp

--- app/model/article.php
+++ app/model/article.php
@@ -1,21 +1,60 @@
 <?php
-    class Article{
-        private $ArtID;
-        private $CatID;
-        private $TimeModify;
-        private $StaffID;
-        private $Title;
+class Article {
+    private $ArtID;
+    private $CatID;
+    private $TimeModify;
+    private $StaffID;
+    private $Title;
 
-        function __construct($ArtID, $CatID, $TimeModify, $StaffID, $Title)
-        {
-            $this->ArtID = $ArtID;
-            $this->CatID = $CatID;
-            $this->TimeModify = $TimeModify;
-            $this->StaffID = $StaffID;
-            $this->Title = $Title;
-
-        }
-        
+    public function __construct($ArtID, $CatID, $TimeModify, $StaffID, $Title)
+    {
+        $this->ArtID = $ArtID;
+        $this->CatID = $CatID;
+        $this->TimeModify = $TimeModify;
+        $this->StaffID = $StaffID;
+        $this->Title = $Title;
     }
 
+    // Getters
+    public function getArtID() {
+        return $this->ArtID;
+    }
+
+    public function getCatID() {
+        return $this->CatID;
+    }
+
+    public function getTimeModify() {
+        return $this->TimeModify;
+    }
+
+    public function getStaffID() {
+        return $this->StaffID;
+    }
+
+    public function getTitle() {
+        return $this->Title;
+    }
+
+    // Setters
+    public function setArtID($ArtID) {
+        $this->ArtID = $ArtID;
+    }
+
+    public function setCatID($CatID) {
+        $this->CatID = $CatID;
+    }
+
+    public function setTimeModify($TimeModify) {
+        $this->TimeModify = $TimeModify;
+    }
+
+    public function setStaffID($StaffID) {
+        $this->StaffID = $StaffID;
+    }
+
+    public function setTitle($Title) {
+        $this->Title = $Title;
+    }
+}
 ?>
