3c3f42ac1ea74ed4681ed59b466cf4d2727d0e72
Cuongpipe <126488715+cuonglqc@users.noreply.github.com>
2025-04-28 19:55:08 +0700

    Update database.php
    thêm  mysqli_set_charset($conn, "utf8"); vào file kết nối database

--- app/config/database.php
+++ app/config/database.php
@@ -6,6 +6,7 @@
         {
             $conn = mysqli_connect("localhost", "root", "") or die("Hem kết nối được");
             mysqli_select_db( $conn,"BinhDinhNews", ) or die("Đéo tìm thấy da");
+            mysqli_set_charset($conn, "utf8");
             return $conn;
         }
         public static function closeConnection($conn)
