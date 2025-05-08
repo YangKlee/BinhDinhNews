0777ae06a262172a743dd5c564bc89f558df51f4
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-05-08 15:59:17 +0700

    test

---
+++ app/views/left/menu-admin.php
@@ -0,0 +1,20 @@
+<div class="menu-container">
+    <div class="user-info-container">
+                <img src="../images/user.png" alt="">
+                <h3 class="username-tx">Username
+                </h3>
+
+    </div>
+            <hr>
+    <ul class="function-list-container">
+                <li>
+                    <a href="">THÔNG TIN CÁ NHÂN</a>
+                </li>
+                <li>
+                    <a href="">BÀI BÁO</a>
+                </li>
+                <li>
+                    <a href="../../app/controller/dangxuat.php">ĐĂNG XUẤT</a>
+                </li>
+    </ul>
+</div>
--- public/Admin/index.php
+++ public/Admin/index.php
@@ -4,22 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý</title>
+    <link rel="stylesheet" href="../css/index-admin.css">
     <link rel="stylesheet" href="../css/menu-admin.css">
     <link rel="stylesheet" href="../css/reset.css">
     <style>
 
   </style>
 </head>
-<body>
-    <div class="left-container">
-        <div class="user-info-container">
-            <img src="../images/user.png" alt="">
-            <h3 class="username-tx">Username
-            </h3>
-        </div>
-    </div>
-    <div class="right-container">
+<style>
+    .main-container{
+        display: grid;
 
+        grid-template-columns: 20% 80%;
+        grid-template-rows: 100%;
+    }
+</style>
+<body>
+
+    <div class="main-container">
+        <div class="left-container">
+            <?php
+                include "../../app/views/left/menu-admin.php";
+            ?>
+
+        </div>
+        <div class="right-container">
+            <h1>Chào mừng Username</h1>
+            <h1>Trang quản lý cho nhà báo</h1>
+        </div>
     </div>
 </body>
 </html>
