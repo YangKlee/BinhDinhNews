ae275cf9f725700cd5e5337d04109e79d62f3459
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-05-14 10:26:29 +0700

    life

---
+++ app/controller/submitBaiBao.php
@@ -0,0 +1,25 @@
+<?php
+    require "../../app/controller/loadsession.php"
+
+?>
+<?php
+    if($_SESSION['role'] < 1)
+    {
+        echo "Người dùng không hợp lệ"; 
+    }
+?>
+<?php
+    /**
+     * Quy trình xử lý
+     * 1. Nhận data từ phương thức post của page newArticle.php
+     * 2.Insert phần tiêu đề, tag, ... vào database
+     * 3.Lấy ID vừa insert, tạo floder với tên ID đó ở mục ArticleData và UploadImages
+     * 4. Lưu bài báo và hình ảnh vào các mục trên, đối với hình ảnh thì cộng thêm time() để tránh trùng lặp
+     * 5. Lưu các tên hình ảnh đó vô databases
+     */
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+
+    }
+
+?>
---
+++ app/model/articleDAOAdmin.php
@@ -0,0 +1,40 @@
+<?php
+    // Gọi thư viện đã xây sẵn
+    require_once '../config/database.php';
+    class articleDAO{
+        // get connection thông qua lớp Database Connection để tiện lợi hơn
+        function getConnection()
+        {
+            $dbConnect =  new DatabaseConnection();
+            return $dbConnect->getConnection();
+        }
+        // lấy dữ liệu bài báo và trả về dạng  ???
+        function getListArticleQuery($query)
+        {
+            // get connection
+            $conn = $this->getConnection();
+            // thực hiện query từ chuỗi query tham số
+            $kqua = mysqli_query($conn,$query);
+            // đóng kết nối
+            mysqli_close($conn);
+
+            return $kqua;
+
+
+        }
+        function getArticle($id)
+        {
+            $conn = $this->getConnection();
+            // thực hiện query từ chuỗi query tham số
+            $kqua = mysqli_query($conn,"Select * from Article where ArticleID = ".$id."");
+            // đóng kết nối
+            mysqli_close($conn);
+
+            return $kqua;
+        }
+        function addArticleHeader($title, $tag,  $authorID, $authorGuestName ,$catID )
+        {
+            
+        }
+    }
+?>
