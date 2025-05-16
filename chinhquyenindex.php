b6a560cc6d4447943e4f9b606533a406e102535d
Tlinh_nbnp2305 <pttlinh020305@gmail.com>
2025-05-16 14:45:13 +0700

    trang chủ chính quyền

---
+++ chinhquyenindex.php
@@ -0,0 +1,92 @@
+<head>
+    
+    <link rel="stylesheet" href="cssChinhquyen/chinhquyen.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+</head>
+<div class="col-left col-xs-12 col-md-9 col-sm-8">    
+    <div id="module11" class="ModuleWrapper  table-trangtrong1">
+        <div id="module12" class="ModuleWrapper">
+            <div class="pathway-category">
+                <nav class="breadcrumb" aria-label="breadcrumb">
+                    <span class="breadcrumb-item">
+                        <a href="http://localhost/BinhDinhNews/public/index.php" title="Trang chủ">
+                            <i class="fas fa-home"></i> Trang chủ
+                        </a>
+                    </span>
+                    <span class="breadcrumb-separator">/</span>
+                    <span class="breadcrumb-item">
+                        <a href="http://localhost/chinhquyen/chinhquyenindex.php" title="Chính quyền">Chính quyền</a>
+                    </span>
+                </nav>
+            </div>
+        </div>
+        <div id="module13" class="ModuleWrapper" modulerootid="1"> 
+            <div class="category-image-type-bd">   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/indextinhuy.php" title="Tỉnh ủy"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/tinhuy.jpg" alt="Tỉnh ủy" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">Tỉnh ủy</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/indexhdnd.php" title="HĐND tỉnh"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/hdnd.jpg" alt="HĐND tỉnh" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">HĐND tỉnh</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/ubnd.php" title="UBND tỉnh"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/ubnd.jpg" alt="UBND tỉnh" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">UBND tỉnh</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/ubmttqvn.php" title="UBMTTQVN tỉnh"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/ubmt.jpg" alt="UBMTTQVN tỉnh" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">UBMTTQVN tỉnh</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/coquanchuyenmon.php" title="Cơ quan chuyên môn"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/cqcm.jpg" alt="Cơ quan chuyên môn" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">Cơ quan chuyên môn</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/indexubnd.php" title="UBND các huyện, thị xã, thành phố"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/huyen.jpg" alt="UBND các huyện, thị xã, thành phố" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">UBND các huyện, thị xã, thành phố</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/indexsunghiep.php" title="Đơn vị Sự nghiệp"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/dvsn.jpg" alt="Đơn vị Sự nghiệp" style="height: 25px;>
+                        </div> 
+                        <div class="article-info">Đơn vị Sự nghiệp</div> 
+                    </a> 
+                </div>   
+                <div class="item-category clearfix"> 
+                    <a class="article-img" href="http://localhost/chinhquyen/hiepquan.php" title="Đơn vị Hiệp quản"> 
+                        <div class="img-cate"> 
+                            <img class="post-image" src="imgChinhquyen/logo/dvhq.jpg" alt="Đơn vị Hiệp quản"style="height: 25px;>
+                        </div> 
+                        <div class="article-info">Đơn vị Hiệp quản</div> 
+                    </a> 
+                </div>   
+            </div>  
+        </div>
+    </div>  
+</div>
