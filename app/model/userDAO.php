1b1c2b009760725825cb24283161b84935405425
Tlinh_nbnp2305 <pttlinh020305@gmail.com>
2025-05-24 22:05:06 +0700

    listuser

--- app/model/userDAO.php
+++ app/model/userDAO.php
@@ -2,7 +2,12 @@
     require_once $_SERVER['DOCUMENT_ROOT'].'/BinhDinhNews/app/config/database.php';
 
     class UserDAO{
-        
+        private $conn;
+
+        public function setConnection($conn) {
+        $this->conn = $conn;
+    }
+
         function getConnection()
         {
             $dbConnect =  new DatabaseConnection();
@@ -72,6 +77,40 @@
              }
              mysqli_close($conn);
              return false;
+        }
+
+        public function isUsernameOrEmailExists($username, $email) {
+                $sql = "SELECT * FROM userdata WHERE UserName = ? OR Email = ?";
+                $stmt = $this->conn->prepare($sql);
+                if (!$stmt) {
+                    die("Prepare failed: " . $this->conn->error);
+                }
+                $stmt->bind_param("ss", $username, $email);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                return $result->num_rows > 0;
+            }
+
+        public function createUser($username, $email, $role, $img) {
+            $sql = "INSERT INTO userdata (UserName, Email, Role, user_img) VALUES (?, ?, ?, ?)";
+            $stmt = $this->conn->prepare($sql);
+            if (!$stmt) {
+                die("Prepare failed: " . $this->conn->error);
+            }
+            $stmt->bind_param("ssis", $username, $email, $role, $img);
+            return $stmt->execute();
+        }
+
+        public function getAllUsers() {
+            $conn = $this->getConnection();
+            $sql = "SELECT UserID, UserName, Role, user_img FROM userdata ";
+            $result = $conn->query($sql);
+            $users = [];
+            while($row = $result->fetch_assoc()) {
+                $users[] = $row;
+            }
+            $conn->close();
+            return $users;
         }
         
         function updateUserPassword($uid, $newPassword)
