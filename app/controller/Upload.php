f5dfb503d09d0e0b5ed9fdeacb68fc5741c7b8e2
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-05-24 10:21:26 +0700

    Update Upload.php

--- app/controller/Upload.php
+++ app/controller/Upload.php
@@ -13,6 +13,7 @@
 
             $newFileName = time() ."_".$uid. "_" . basename($file['name']);
             $targetPath = $_SERVER['DOCUMENT_ROOT'] . "/BinhDinhNews/public/images/userAvatar/".$newFileName;
+            
             move_uploaded_file($file['tmp_name'], $targetPath );
             return $newFileName;
         }
