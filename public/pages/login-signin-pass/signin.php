8e09d5b3519374f6eeb2e07bcf8808448d84a783
Cường <tranthanhcuongit@gmail.com>
2025-06-05 12:50:25 +0700

    Sửa chút chút cho hay

--- public/pages/login-signin-pass/signin.php
+++ public/pages/login-signin-pass/signin.php
@@ -1,25 +1,35 @@
 <?php 
-    $nofi = "";
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        if(isset($_POST['username_tb']) && $_POST['username_tb'] != "" &&
-        isset($_POST['password_tb']) && $_POST['password_tb'] != "" &&
-        isset($_POST['email_tb']) && $_POST['email_tb'] != "" &&
-        $_POST['password_tb'] == $_POST['repassword_tb'] )
-        {
-            require "../../../app/config/database.php";
-            $db = new DatabaseConnection();
-            $conn = $db->getConnection();
-            $query= "Insert into UserData(UserName,PassWord,Email,ROLE) VALUES(?, ? , ?, 0)";
+        require "../../../app/config/database.php";
+        $db = new DatabaseConnection();
+        $conn = $db->getConnection();
+
+        $username = trim($_POST['username_tb']);
+        $email = trim($_POST['email_tb']);
+        $password = $_POST['password_tb'];
+
+        // Kiểm tra trùng username hoặc email
+        $checkQuery = "SELECT * FROM UserData WHERE UserName = ? OR Email = ?";
+        $checkStmt = mysqli_prepare($conn, $checkQuery);
+        mysqli_stmt_bind_param($checkStmt, "ss", $username, $email);
+        mysqli_stmt_execute($checkStmt);
+        $result = mysqli_stmt_get_result($checkStmt);
+
+        if(mysqli_num_rows($result) > 0) {
+            echo '<div class="error">Tên tài khoản hoặc email đã tồn tại</div>';
+        } else {
+            // Hash password an toàn hơn
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $query= "INSERT INTO UserData(UserName,PassWord,Email,ROLE) VALUES(?, ?, ?, 0)";
             $sttm = mysqli_prepare($conn,$query);
-            mysqli_stmt_bind_param($sttm, "sss", $_POST['username_tb'], hash("sha256", $_POST['password_tb']), $_POST['email_tb'] );
-            mysqli_stmt_execute($sttm);
-            echo '<script> alert("Đăng ký thành công"); </script>';
-            header("Location: login.php");
-            exit();
-        }
-        else{
-            echo '<script>alert("Đăng ký thất bại, vui lòng kiểm tra lại thông tin")</script>';
+            mysqli_stmt_bind_param($sttm, "sss", $username, $hashedPassword, $email );
+            if(mysqli_stmt_execute($sttm)){
+                echo '<script>alert("Đăng ký thành công"); window.location.href="login.php";</script>';
+                exit();
+            } else {
+                echo '<script>alert("Đăng kí thất bại");</script>';
+            }
         }
     }
 ?>
@@ -50,7 +60,7 @@
         <div class="password-warper">
             <label for="password">Mật khẩu <span style="color: red;"> *</span></label>
             <div class="input-icon-wrapper">
-                <input type="password"  id="password" name="password_tb" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Mật khẩu có ít nhất 8 kí tự và có 1 kí tự đặt biệt và 1 chữ hoa và 1 chữ thường" required>
+                <input type="password"  id="password" name="password_tb" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Mật khẩu có ít nhất 8 kí tự và 1 chữ hoa và 1 chữ thường" required>
                 <span class="icon-eye">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-eye-inactive" style="display:none;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
