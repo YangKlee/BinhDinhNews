eb7251da66b7102b678533a2af486ac17f547fc6
Cường <tranthanhcuongit@gmail.com>
2025-06-06 22:34:03 +0700

    Không cho làm lanh coppy paste

--- public/pages/login-signin-pass/signin.php
+++ public/pages/login-signin-pass/signin.php
@@ -60,7 +60,7 @@
         <div class="password-warper">
             <label for="password">Mật khẩu <span style="color: red;"> *</span></label>
             <div class="input-icon-wrapper">
-                <input type="password"  id="password" name="password_tb" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Mật khẩu có ít nhất 8 kí tự và 1 chữ hoa và 1 chữ thường" required>
+                <input type="password"  id="password" name="password_tb" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Mật khẩu có ít nhất 8 kí tự và 1 chữ hoa và 1 chữ thường" required oncopy="return false;" onpaste="return false;" oncut="return false;">
                 <span class="icon-eye">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-eye-inactive" style="display:none;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
@@ -75,7 +75,7 @@
         <div class="repassword-warper">
             <label for="password">Xác nhận mật khẩu <span style="color: red;"> *</span></label>
             <div class="input-icon-wrapper">
-                <input type="password" id="repassword" name="repassword_tb" required>
+                <input type="password" id="repassword" name="repassword_tb" required oncopy="return false;" onpaste="return false;" oncut="return false;">
                 <span class="icon-eye">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-eye-inactive" style="display:none;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
