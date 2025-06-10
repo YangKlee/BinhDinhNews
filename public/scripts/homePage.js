66c7f267ee56b94cdea80d0c49621ddbe59bdc3d
YangKlee <147032564+YangKlee@users.noreply.github.com>
2025-06-10 11:07:25 +0700

    quên hẹ hẹ hẹ

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -1,6 +1,6 @@
-// var timerHotNews = setInterval(() => {
-//     loadArtNext(true)
-// }, 10000);
+var timerHotNews = setInterval(() => {
+    loadArtNext(true)
+}, 10000);
 var timerThoiSu =setInterval(() => {
     loadArtThoiSuNext(true)
 }, 3000);
